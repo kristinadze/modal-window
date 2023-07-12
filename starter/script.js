f2e83a7edf1052d9e0bf3e606886fa116ceb3582
Kristina Dzelebdzic <kristinaar94@gmail.com>
2023-07-12 16:56:04 -0700

    esc-keydown-added

--- starter/script.js
+++ starter/script.js
@@ -19,4 +19,10 @@ for (let i = 0; i < btsOpenModal.length; i++) {
 
   btnCloseModal.addEventListener('click', closeModal);
   overlay.addEventListener('click', closeModal);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
 }
