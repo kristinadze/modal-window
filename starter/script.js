cf7a77d48d38f484d9a8d195b33d3cd8f485626d
Kristina Dzelebdzic <kristinaar94@gmail.com>
2023-07-11 21:12:47 -0700

    selected all the elements, created btns

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btsOpenModal);
+
+for (let i = 0; i < btsOpenModal.length; i++) {
+  console.log(
+    btsOpenModal[i].addEventListener('click', function () {
+      console.log('Button clicked');
+      modal.classList.remove('hidden');
+    })
+  );
+}
